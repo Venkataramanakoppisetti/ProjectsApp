768f6383b5bdcdd8b51ec5be7f153e4c22f5b768
Venkata <venkataramanakoppisetti25@gmail.com>
2023-10-15 11:40:40 +0000

    Projects

---
+++ src/components/Header/index.js
@@ -0,0 +1,33 @@
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="nav-content">
+      <img
+        src="https://res.cloudinary.com/dyhrvktyr/image/upload/v1695882331/evnky_ev6jih.jpg"
+        alt="social"
+        className="website-logo"
+      />
+    </div>
+    <div className="nav-menu">
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/124/124010.png"
+        className="social-network-img"
+        alt="social"
+      />
+      <img
+        src="https://cdn.iconscout.com/icon/free/png-512/free-whatsapp-1868967-1583131.png?f=webp&w=256"
+        className="social-network-img"
+        alt="social"
+      />
+
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXaNFhHN3tIzDCWjaI7wT_v8F-vZcq5vbPex8XsXv2Zw&s"
+        className="social-network-img"
+        alt="social"
+      />
+    </div>
+  </nav>
+)
+
+export default Header
